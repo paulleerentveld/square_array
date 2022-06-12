b70587e39c0316207727bdf17cbe4a2fdfb5309e
paulleerentveld <paul@veldnet.com.au>
2022-06-12 14:47:19 +1000

    lab done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
 def square_array(array)
-  # your code here
+  newarray = []
+  array.each do |number| 
+    newarray << number**2
+  end
+  return newarray
 end
+
+#square_array([1,2,3])
+
+def square_array_collect(array)
+  new_array = array.collect do |number|
+    number ** 2
+  end
+  return new_array
+end
+
+#square_array_collect([1,2,3])
